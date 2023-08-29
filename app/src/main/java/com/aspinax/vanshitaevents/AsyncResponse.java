ccf74f4811b2f35c220c95b72e65224bde5abdcb
Vanshiii29 <138807197+Vanshiii29@users.noreply.github.com>
2023-08-29 21:36:49 +0530

    Add files via upload

---
+++ app/src/main/java/com/aspinax/vanshitaevents/AsyncResponse.java
@@ -0,0 +1,6 @@
+import java.util.Map;
+
+public interface AsyncResponse {
+    void resultHandler(Map<String, Object> result, int resultCode);
+    void resultHandler(String msg, int resultCode);
+}
---
+++ app/src/main/java/com/aspinax/vanshitaevents/CaptureActivityPortrait.java
@@ -0,0 +1,3 @@
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity { }
